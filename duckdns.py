2e6bb1ac75a302260947377a41df0c920cf3d1ff
longhd2 <43842525+longhd2@users.noreply.github.com>
2023-04-27 05:06:06 +0700

    Update duckdns.py

--- duckdns.py
+++ duckdns.py
@@ -1,11 +1,3 @@
-# crontab -e
-# */10 * * * * /usr/bin/python /home/pi/duckdns.py >/dev/null 2>&1
-# xem log: sudo grep CRON /var/log/syslog
-# Apr 27 10:00:01 host CRON[1234]: (root) CMD (/usr/bin/python3 /home/pi/update_ip_duckdns/duckdns.py >> /home/pi/cron.log 2>&1)
-# Mở terminal trên Linux.
-# cd /home/pi/update_ip_duckdns
-# chmod +x duckdns.py
-
 import requests
 import os
 
